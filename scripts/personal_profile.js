0136df9fcaee1ca0d65bf978fc9be648c66a8f79
suncihai <suncihai@gmail.com>
2016-10-10 13:49:44 -0400

    add doraemon app mobile version

--- scripts/personal_profile.js
+++ scripts/personal_profile.js
@@ -5,6 +5,8 @@ window.onload=function(){
   var russiablock=document.getElementById('russiablock');
   var resume=document.getElementById('resumeBody');
   var heart=document.getElementById('heart');
+  var mobile=document.getElementById('mobile');
+  var container=document.getElementsByClassName('container')[0];
   var homebtn=document.getElementById('homebtn');
   var content=document.getElementById('content');
   var news=document.getElementsByClassName('news');
@@ -12,6 +14,8 @@ window.onload=function(){
   var newsApp=document.getElementById('newsApp');
   var newsApp_icon=document.getElementById('newsApp_icon');
   var minionsApp=document.getElementById('minionsApp');
+  var doraemongameApp=document.getElementById('doraemongameApp');
+  var doraemongameApp_icon=document.getElementById('doraemongameApp_icon');
   var closeApp=false;
   var PixelChange=false;
 
@@ -712,39 +716,74 @@ window.onload=function(){
   RandomBlock();
 
   //iphone apps
+  var appOn=1;
   newsApp_icon.style.transition="ease 1s";
+  resumeBody.style.transition="ease 1s";
+  mobile.style.transition="ease 1s";
+  container.style.transition="ease 1s";
   news[0].style.transition="ease 1s";
   news[1].style.transition="ease 1s";
   news[2].style.transition="ease 1s";
   minionsApp.style.transition="ease 1s";
+  doraemongameApp.style.transition="ease 1s";
+  doraemongameApp_icon.style.transition="ease 1s";
 
   homebtn.onclick=function(){
     closeApp=true;
     minionsApp.style.transition="ease 1s";
-    for(var i=0;i<news.length;i++){
-      news[i].style.width="0px";
-      news[i].style.height="0px";
-      news[i].style.background="";
-      news[i].innerText="";
+    doraemongameApp.style.transition="ease 1s";
+    doraemongameApp_icon.style.transition="ease 1s";
+    switch(appOn){
+      case 1:
+         for(var i=0;i<news.length;i++){
+            news[i].style.width="0px";
+            news[i].style.height="0px";
+            news[i].style.background="";
+            news[i].innerText="";
+          }
+          break;
+      case 2:
+          doraemongameApp.style.width="0px";
+          doraemongameApp.style.height="0px";
+          doraemongameApp.style.background="";
+          doraemongameApp.innerHTML="";
+          $("#resumeBody").removeClass('col-md-3').addClass('col-md-6');
+          $("#mobile").removeClass('col-md-6').addClass('col-md-3');
+          container.style.transform="rotate(0deg)";
+          doraemongameApp.style.transform="rotate(0deg)";
+          $("#homebtn_des").css("left","");
+          break;
+      default:
+          break;
     }
+
     inner_container.style.overflow="hidden";
-    setTimeout(function(){
-      newsApp_icon.style.width="70px";
-      newsApp_icon.style.height="70px";
-      newsApp_icon.style.marginLeft="10px";
-      newsApp_icon.style.marginTop="20px";
-      newsApp_icon.style.background="url(images/news_icon.png)"
-      minionsApp.style.width="70px";
-      minionsApp.style.height="70px";
-      minionsApp.style.left="90px";
-      minionsApp.style.top="0px";
-      minionsApp.style.background="url(images/minions_icon.png)";
+      setTimeout(function(){
+        newsApp_icon.style.width="70px";
+        newsApp_icon.style.height="70px";
+        newsApp_icon.style.marginLeft="10px";
+        newsApp_icon.style.marginTop="20px";
+        newsApp_icon.style.background="url(images/news_icon.png)"
+        minionsApp.style.width="70px";
+        minionsApp.style.height="70px";
+        minionsApp.style.left="90px";
+        minionsApp.style.top="0px";
+        minionsApp.style.background="url(images/minions_icon.png)";
+        doraemongameApp_icon.style.width="70px";
+        doraemongameApp_icon.style.height="70px";
+        doraemongameApp_icon.style.left="180px";
+        doraemongameApp_icon.style.top="0px";
+        doraemongameApp_icon.style.background="url(images/doraemongame_icon.png)";
     },100);
+
+    appOn=0;
   }
 
   newsApp_icon.onclick=function(){
+    appOn=1;
     if(closeApp){
       minionsApp.style.transition="0s";
+      doraemongameApp_icon.style.transition="0s";
       setTimeout(function(){
         newsApp_icon.style.width="";
         newsApp_icon.style.height="";
@@ -771,6 +810,8 @@ window.onload=function(){
         news[2].style.background="url(images/shopping.gif)";
         minionsApp.style.width="0px";
         minionsApp.style.height="0px";
+        doraemongameApp_icon.style.width="0px";
+        doraemongameApp_icon.style.height="0px";
         inner_container.style.overflowY="scroll";
       },100);
 
@@ -788,5 +829,28 @@ window.onload=function(){
       
       closeApp=false;
     }
+  }
+
+  doraemongameApp_icon.onclick=function(){
+    appOn=2;
+    if(closeApp){
+      doraemongameApp_icon.style.width="";
+      doraemongameApp_icon.style.height="";
+      doraemongameApp_icon.style.marginLeft="";
+      doraemongameApp_icon.style.marginTop="";
+      doraemongameApp_icon.style.background="";
+      doraemongameApp.style.width="320px";
+      doraemongameApp.style.height="567px";
+      doraemongameApp.style.left="-124px";
+      doraemongameApp.style.top="-143px";
+      doraemongameApp.innerHTML='<object type="text/html" width="567px" height="320px" data="http://suncihai.x10host.com/doraemon_game_mobile/doraemon_game_mobile.html"></object>';
+
+      $("#resumeBody").removeClass('col-md-6').addClass('col-md-3');
+      $("#mobile").removeClass('col-md-3').addClass('col-md-6');
+      container.style.transform="rotate(-90deg)";
+      doraemongameApp.style.transform="rotate(90deg)";
+      $("#homebtn_des").css("left","35%");
+    }
+    closeApp=false;
   }
 }
