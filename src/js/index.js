837b1161cba070ec3b46d024bfc47eb15f9f8275
suncihai <suncihai@gmail.com>
2016-11-29 14:33:40 -0500

    debugging

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
 (function(){
-   // var desaturate = false;
-   // desaturator();
+   var desaturate = false;
+   desaturator();
 
-   // function desaturator(){
-   // 	  if(!desaturate){
-   // 	  	 grayscale($(".resume_header"));
-   // 	  	 desaturate = true;
-   // 	  }else{
-   //       grayscale.reset($(".resume_header"));
-   //       desaturate = false;
-   // 	  }
-   //    setTimeout(desaturator,3100);
-   // }
+   function desaturator(){
+   	  if(!desaturate){
+   	  	 grayscale($(".resume_header"));
+   	  	 desaturate = true;
+   	  }else{
+         grayscale.reset($(".resume_header"));
+         desaturate = false;
+   	  }
+      setTimeout(desaturator,3100);
+   }
 })()
