d3f4a27e16260b8e01477369efc9b76855c56b4a
cihai sun <cihai sun>
2017-05-09 00:16:21 -0400

    add

--- src/js/apps.js
+++ src/js/apps.js
@@ -4,39 +4,6 @@ window.onload=function(){
      $("#diarypage").fadeIn();
   });
 
-   $("#locker").click(function(event){
-   	 $("#diary_enter").fadeOut();
-  	 $("#diaryhint").fadeOut();
-  	 clearInterval(diarybooktimer);
-  	 diarybooktimer=null;
-  	 diarybookboolean=false;
-   	 if(!lockerboolean){
-   	 	event.stopPropagation();
-	  	 var $that = $(this);
-	  	 if(!lockertimer){
-	  	 	lockertimer = setInterval(function(){
-		        $that.animate({
-			        bottom:"40px"
-			  	 },300);
-			  	 setTimeout(function(){
-			        $that.animate({
-				        bottom:"25px"
-				  	 },300);
-			  	 },300);
-		  	 },600);
-	  	 }
-	  	 $("#lockerhint").fadeIn();
-	  	 $("#locker_enter").fadeIn();
-	  	 lockerboolean=true;
-	  	}else{
-           clearInterval(lockertimer);
-  	       lockertimer=null;
-  	       $("#locker_enter").fadeOut();
-  	       $("#lockerhint").fadeOut();
-  	       lockerboolean=false;
-	  	}
-  });
-
   $("#backtohomebtn1").click(function(){
      $("#diarypage").hide();
      $("#homebody").fadeIn();
